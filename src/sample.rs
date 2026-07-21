2103809618bfa0a168dc94428b4665dff9811900
alessiamore <alessiamore@proton.me>
2026-07-21 18:36:41 +0300

    Create sample.rs

---
+++ src/sample.rs
@@ -0,0 +1,15 @@
+use crate::image::ImageFile;
+
+pub fn load() -> ImageFile {
+
+    ImageFile {
+
+        name: String::from("mountains.jpg"),
+
+        width: 1920,
+
+        height: 1080
+
+    }
+
+}
